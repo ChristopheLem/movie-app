dcc9124b5d43729eeb8975abb1d2ebe0791836e2
ChristopheLem <lemercier60@gmail.com>
2020-06-26 14:34:29 +0200

    add bootstrap 4 in the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Header, MovieList, MovieDetails } from './components';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <div className="d-flex flex-row" >
+          <MovieList />
+          <MovieDetails />
+        </div>
       </div>
     );
   }
