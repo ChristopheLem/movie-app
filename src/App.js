7158d1c6c17ca1e4f99a399ef259ee52af17ad9f
ChristopheLem <lemercier60@gmail.com>
2020-06-26 14:47:18 +0200

    set components MovieElement, MovieList and MovieDetails

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Header, MovieList, MovieDetails } from './components';
+import { Header, MovieList, MovieDetails } from './components/index';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App d-flex flex-column">
         <Header />
-        <div className="d-flex flex-row" >
+        <div className="d-flex flex-row flex-fill pt-4 p-2" >
           <MovieList />
           <MovieDetails />
         </div>
